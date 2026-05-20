a3150a6e99ba542a104d192b1f26c9529405f69f
Ramon Luz <ramon.luz20@hotmail.com>
2026-05-20 10:02:49 -0300

    Primeiro commit

---
+++ src/main/java/com/livrotech/LivrotechApplication.java
@@ -0,0 +1,13 @@
+package com.livrotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrotechApplication.class, args);
+	}
+
+}
