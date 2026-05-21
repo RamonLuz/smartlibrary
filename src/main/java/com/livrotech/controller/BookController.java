fb627e95851ac388a1c96d7b280f18a76e631ebf
Ramon Luz <ramon.luz20@hotmail.com>
2026-05-21 11:19:44 -0300

    Adicinando endpoints books

--- src/main/java/com/livrotech/controller/BookController.java
+++ src/main/java/com/livrotech/controller/BookController.java
@@ -1,9 +1,14 @@
 package com.livrotech.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +18,7 @@ import com.livrotech.service.BookService;
 
 @RestController
 @RequestMapping("/Books")
-public class BookController { 
+public class BookController {
 
     private final BookService livroService;
 
@@ -22,13 +27,40 @@ public class BookController {
     }
 
     @GetMapping
-    public List<Book> listar() {
-        return livroService.listar();
+    public ResponseEntity<List<Book>> listar() {
+        List<Book> livros = livroService.listar();
+        return ResponseEntity.ok(livros);
     }
 
     @PostMapping
-    public Book salvar(@RequestBody Book livro) {
-    	
-        return livroService.salvar(livro);
+    public ResponseEntity<Book> salvar(@RequestBody Book livro) {
+    	Book livroSalvo = livroService.salvar(livro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroSalvo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> buscarPorId(@PathVariable Long id) {
+
+        Optional<Book> livro = livroService.buscarPorId(id);
+
+        if (livro.isPresent()) {
+            return ResponseEntity.ok(livro.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> atualizar(
+            @PathVariable Long id,
+            @RequestBody Book livro) {
+
+        Optional<Book> livroAtualizado = livroService.atualizar(id, livro);
+
+        if (livroAtualizado.isPresent()) {
+            return ResponseEntity.ok(livroAtualizado.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/livrotech/service/BookService.java
+++ src/main/java/com/livrotech/service/BookService.java
@@ -1,6 +1,7 @@
 package com.livrotech.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -23,5 +24,39 @@ public class BookService {
     public Book salvar(Book livro) {
     	
         return livroRepository.save(livro);
+    }
+    
+    public Optional<Book> buscarPorId(Long id) {
+        return livroRepository.findById(id);
+    }
+    
+    public Optional<Book> atualizar(Long id, Book livroAtualizado) {
+
+        Optional<Book> livroExistente = livroRepository.findById(id);
+
+        if (livroExistente.isPresent()) {
+
+        	Book livro = livroExistente.get();
+
+            livro.setTitulo(livroAtualizado.getTitulo());
+            livro.setAutor(livroAtualizado.getAutor());
+            livro.setPreco(livroAtualizado.getPreco());
+
+            livroRepository.save(livro);
+
+            return Optional.of(livro);
+        }
+
+        return Optional.empty();
+    }
+    
+    public boolean deletar(Long id) {
+
+        if (livroRepository.existsById(id)) {
+            livroRepository.deleteById(id);
+            return true;
+        }
+
+        return false;
     }
 }
